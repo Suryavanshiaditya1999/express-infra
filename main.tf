7f77352a11670c1720efa5ad05b236221c62e9b0
Suryavanshiaditya1999 <suryvanshiaditya099@gmail.com>
2024-06-23 06:03:12 +0530

    deploy and main.tf updated

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "myuniquebucket1222"
+    key    = "aws/vpc-deploy/terraform.tfstate"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
